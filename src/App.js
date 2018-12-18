24d8bc227d12c2438a9afcde557ce7fea6926702
dipen bagia <dipen.bagia@klarna.com>
2018-12-18 22:04:28 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +19,15 @@ class App extends Component {
           >
             Learn React
           </a>
+        </header>*/}
+        <header className="masthead bg-primary text-white text-center">
+          <div className="container">
+            <img className="img-fluid mb-5 d-block mx-auto" width="256px" height="256px"
+                 src="img/myAvatar.png" alt=""/>
+              <h1 className="text-uppercase mb-0">Avantika Devraj</h1>
+              <hr className="star-light"/>
+                <h2 className="font-weight-light mb-0">User Experience Designer - Interaction Designer</h2>
+          </div>
         </header>
       </div>
     );
