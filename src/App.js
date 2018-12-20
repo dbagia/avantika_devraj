0448404c78c6ae4f45408116021b98991ab476d0
dipen bagia <dipen.bagia@klarna.com>
2018-12-20 22:31:03 +0100

    added start icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
 
 class App extends Component {
   render() {
+    const hrClasses = ['star-light', 'App-absolute-center'].join(' ')
+    const starIconClasses = ['fa-layers', 'fa-fw', 'App-absolute-center'].join(' ')
     return (
       <div className="App">
         {/*<header className="App-header">
@@ -24,9 +26,15 @@ class App extends Component {
           <div className="container">
             <img className="img-fluid mb-5 d-block mx-auto" width="256px" height="256px"
                  src="img/myAvatar.png" alt=""/>
-              <h1 className="text-uppercase mb-0">Avantika Devraj</h1>
-              <hr className="star-light"/>
-                <h2 className="font-weight-light mb-0">User Experience Designer - Interaction Designer</h2>
+            <h1 className="text-uppercase mb-0">Avantika Devraj</h1>
+            <div style={{position:"relative", marginTop:"2rem", marginBottom:"3.5rem"}}>
+              <hr className={hrClasses} />
+              <span className={starIconClasses} style={{right: "2rem"}}>
+                <FontAwesomeIcon icon={"square"} color="#18BC9B" size={"4x"}/>
+                <FontAwesomeIcon icon={"star"} pull={"center"} size={"2x"} transform={"right-5 shrink-2"}/>
+              </span>
+            </div>
+            <h2 className="mb-0">User Experience Designer - Interaction Designer</h2>
           </div>
         </header>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'typeface-montserrat'
+import 'typeface-lato'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,7 +7,12 @@ import './index.css';
 import './freelancer.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import logo from './logo.svg';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar, faSquare } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faStar)
+library.add(faSquare)
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
